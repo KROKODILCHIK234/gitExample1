75ffe7eba6d3f6005084d3226e27ee0e91333b71
KROKODILCHIK234 <159097510+KROKODILCHIK234@users.noreply.github.com>
2024-03-07 15:20:52 +0800

    Update reporting_test.py

--- Tst/reporting_test.py
+++ Tst/reporting_test.py
@@ -11,51 +11,51 @@ from Src.Logics.json_reporting import json_reporting
 class ReportingTest(unittest.TestCase):
     
     def test_json_reporting_build(self):
-        # Preparation
+        
         data = {storage.unit_key(): [unit_model.create_gram()]}
         report = json_reporting(data)
         
-        # Action
+        
         result = report.create(storage.unit_key())
         
-        # Assertions
+        
         self.assertIsNotNone(result)
         self.assertGreater(len(result), 0)   
     
     def test_reporting_build(self):
-        # Preparation
+        
         data = {storage.unit_key(): [unit_model.create_gram()]}
         
-        # Action
+       
         result = reporting.build(storage.unit_key(), data)
         
-        # Assertions
+        
         self.assertIsNotNone(result)
         self.assertGreater(len(result), 0)
         
     def test_csv_create_unit_key(self):
-        # Preparation
+        
         data = {storage.unit_key(): [unit_model.create_gram()]}
         report = csv_reporting(data)
         
-        # Action
+        
         result = report.create(storage.unit_key())
         
-        # Assertions
+        
         self.assertIsNotNone(result)
         self.assertGreater(len(result), 0)
         
     def test_csv_create_nomenclature_key(self):
-        # Preparation
+        
         data = {
             storage.nomenclature_key(): [nomenclature_model("Тушка бройлера", group_model.create_default_group(), unit_model.create_killogram())]
         }
         report = csv_reporting(data)
         
-        # Action
+        
         result = report.create(storage.nomenclature_key())
         
-        # Assertions
+        
         self.assertIsNotNone(result)
         self.assertGreater(len(result), 0)
         
@@ -63,14 +63,14 @@ class ReportingTest(unittest.TestCase):
             file.write(result)
         
     def test_markdown_create_unit_key(self):
-        # Preparation
+        
         data = {storage.unit_key(): [unit_model.create_gram()]}
         report = markdown_reporting(data)
         
-        # Action
+        
         result = report.create(storage.unit_key())
         
-        # Assertions
+       
         self.assertIsNotNone(result)
         self.assertGreater(len(result), 0)
         
