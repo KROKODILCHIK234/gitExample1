6169e490132d3d43b93b5a80849272a40626f2fa
KROKODILCHIK234 <159097510+KROKODILCHIK234@users.noreply.github.com>
2024-03-18 11:42:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 from flask import Flask
-from storage import storage
-import os
-from logic.data_factory import data_factory
+from Src.settings_manager import settings_manager
+from Src.Storage.storage import storage
+from Src.errors import error_proxy
+from Src.Logics.report_factory import report_factory
+from Src.Logics.start_factory import start_factory
+
+
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
-@app.route("/api/report/<storage_key>/<format>", methods=["GET"])
-def get_report(storage_key: str, format: str):
-    s = storage.storage()
-    if storage_key not in s.get_all_keys():
-        response_type = app.response_class(
-            response=f"Такого ключа не существует",
-            status=500,
-            mimetype="application/text"
-        )
-
-        return response_type
-
-    if format not in [x.split(".")[0][5:] for x in os.listdir("logic/formats")]:
-        response_type = app.response_class(
-            response=f"Ключ существует, но этот формат экспорта недоступен",
-            status=500,
-            mimetype="application/text"
-        )
-        return response_type
+# Сформировать начальный набор данных
+options = settings_manager() 
+start = start_factory(options.settings)
+start.create()
 
 
-    response_type = app.response_class(
-        response=f"{data_factory.create(data_factory(), format, 'Сюда нужно передать данные')}",
-        status=200,
-        mimetype="application/text"
-    )
-
-    return response_type
-
+@app.route("/api/report/<storage_key>", methods = ["GET"])
+def get_report(storage_key: str):
+    """
+        Сформировать отчет
+    Args:
+        storage_key (str): Ключ - тип данных: номенклатура, группы и т.д.
+    """
+    
+    keys = storage.storage_keys( start.storage )
+    if storage_key == "" or  storage_key not in keys:
+        return error_proxy.create_error_response(app, f"Некорректный передан запрос! Необходимо передать: /api/report/<storage_key>. Список ключей (storage_key): {keys}.", 400)
+    
+    # Создаем фабрику
+    report = report_factory()
+    data = start.storage.data
+    
+    # Формируем результат
+    try:
+        result = report.create_response( options.settings.report_mode, data, storage_key, app )  
+        return result
+    except Exception as ex:
+        return error_proxy.create_error_response(app, f"Ошибка при формировании отчета {ex}", 500)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
